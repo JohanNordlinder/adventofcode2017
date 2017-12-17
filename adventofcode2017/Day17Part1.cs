2f59b4baff5b31d0374a047439d6efd6740d3fa1
JohanNordlinder <johan_nordlinder@hotmail.com>
2017-12-17 07:12:59 +0100

    Day 17

---
+++ adventofcode2017/Day17Part1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace adventofcode2017
+{
+    [TestClass]
+    public class Day17Part1
+    {
+        [TestMethod]
+        public void TestRun()
+        {
+            Assert.AreEqual(638, new Program().RunChallenge(3));
+        }
+
+        [TestMethod]
+        public void RealRun()
+        {
+            Console.WriteLine("Result: " + new Program().RunChallenge(314));
+        }
+
+        public class Program
+        {
+            public int RunChallenge(int input)
+            {
+                var numbers = new List<int>();
+                var currentPosition = 0;
+                numbers.Add(0);
+                for(int i = 1; i <= 2017; i++)
+                {
+                    currentPosition = ((currentPosition + input) % numbers.Count()) + 1;
+                    numbers.Insert(currentPosition, i);
+                }
+
+                var result = numbers[numbers.IndexOf(2017) + 1];
+                return result;
+            }
+        }
+    }
+}
---
+++ adventofcode2017/Day17Part2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace adventofcode2017
+{
+    [TestClass]
+    public class Day17Part2
+    {
+        [TestMethod]
+        public void RealRun()
+        {
+            Console.WriteLine("Result: " + new Program().RunChallenge(314));
+        }
+
+        public class Program
+        {
+            public int RunChallenge(int input)
+            {
+                var currentPosition = 0;
+                var count = 1;
+                var lastFirst = -1;
+                for(int i = 1; i <= 50000000; i++)
+                {
+                    currentPosition = ((currentPosition + input) % count) + 1;
+                    count++;
+                    if (currentPosition == 1) {
+                        lastFirst = i;
+                    }
+                }
+
+                return lastFirst;
+            }
+        }
+    }
+}
